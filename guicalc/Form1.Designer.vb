8784ba4acf5a0ee8483d108b9c25345877d20722
frashid17 <frashid274@gmail.com>
2024-03-23 21:55:46 +0300

    Add project files.

---
+++ guicalc/Form1.Designer.vb
@@ -0,0 +1,260 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        btn2 = New Button()
+        btn3 = New Button()
+        btn4 = New Button()
+        btn5 = New Button()
+        btn6 = New Button()
+        btn7 = New Button()
+        btn8 = New Button()
+        btn9 = New Button()
+        btn0 = New Button()
+        btn1 = New Button()
+        btnPlus = New Button()
+        btnMultiply = New Button()
+        btnEquals = New Button()
+        btnMinus = New Button()
+        btnDivide = New Button()
+        btnClear = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn2
+        ' 
+        btn2.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn2.Location = New Point(305, 279)
+        btn2.Name = "btn2"
+        btn2.Size = New Size(91, 80)
+        btn2.TabIndex = 1
+        btn2.Text = "2"
+        btn2.UseVisualStyleBackColor = True
+        ' 
+        ' btn3
+        ' 
+        btn3.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn3.Location = New Point(399, 279)
+        btn3.Name = "btn3"
+        btn3.Size = New Size(93, 80)
+        btn3.TabIndex = 2
+        btn3.Text = "3"
+        btn3.UseVisualStyleBackColor = True
+        ' 
+        ' btn4
+        ' 
+        btn4.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn4.Location = New Point(208, 202)
+        btn4.Name = "btn4"
+        btn4.Size = New Size(99, 75)
+        btn4.TabIndex = 3
+        btn4.Text = "4"
+        btn4.UseVisualStyleBackColor = True
+        ' 
+        ' btn5
+        ' 
+        btn5.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn5.Location = New Point(305, 202)
+        btn5.Name = "btn5"
+        btn5.Size = New Size(91, 75)
+        btn5.TabIndex = 4
+        btn5.Text = "5"
+        btn5.UseVisualStyleBackColor = True
+        ' 
+        ' btn6
+        ' 
+        btn6.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn6.Location = New Point(399, 202)
+        btn6.Name = "btn6"
+        btn6.Size = New Size(93, 75)
+        btn6.TabIndex = 5
+        btn6.Text = "6"
+        btn6.UseVisualStyleBackColor = True
+        ' 
+        ' btn7
+        ' 
+        btn7.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn7.Location = New Point(208, 123)
+        btn7.Name = "btn7"
+        btn7.Size = New Size(99, 77)
+        btn7.TabIndex = 6
+        btn7.Text = "7"
+        btn7.UseVisualStyleBackColor = True
+        ' 
+        ' btn8
+        ' 
+        btn8.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn8.Location = New Point(307, 122)
+        btn8.Name = "btn8"
+        btn8.Size = New Size(89, 77)
+        btn8.TabIndex = 7
+        btn8.Text = "8"
+        btn8.UseVisualStyleBackColor = True
+        ' 
+        ' btn9
+        ' 
+        btn9.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn9.Location = New Point(402, 123)
+        btn9.Name = "btn9"
+        btn9.Size = New Size(90, 76)
+        btn9.TabIndex = 8
+        btn9.Text = "9"
+        btn9.UseVisualStyleBackColor = True
+        ' 
+        ' btn0
+        ' 
+        btn0.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn0.Location = New Point(208, 358)
+        btn0.Name = "btn0"
+        btn0.Size = New Size(99, 80)
+        btn0.TabIndex = 9
+        btn0.Text = "0"
+        btn0.UseVisualStyleBackColor = True
+        ' 
+        ' btn1
+        ' 
+        btn1.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btn1.Location = New Point(209, 279)
+        btn1.Name = "btn1"
+        btn1.Size = New Size(98, 80)
+        btn1.TabIndex = 10
+        btn1.Text = "1"
+        btn1.UseVisualStyleBackColor = True
+        ' 
+        ' btnPlus
+        ' 
+        btnPlus.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnPlus.Location = New Point(493, 123)
+        btnPlus.Name = "btnPlus"
+        btnPlus.Size = New Size(93, 77)
+        btnPlus.TabIndex = 11
+        btnPlus.Text = "+"
+        btnPlus.UseVisualStyleBackColor = True
+        ' 
+        ' btnMultiply
+        ' 
+        btnMultiply.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnMultiply.Location = New Point(494, 279)
+        btnMultiply.Name = "btnMultiply"
+        btnMultiply.Size = New Size(92, 80)
+        btnMultiply.TabIndex = 12
+        btnMultiply.Text = "*"
+        btnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' btnEquals
+        ' 
+        btnEquals.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnEquals.Location = New Point(494, 358)
+        btnEquals.Name = "btnEquals"
+        btnEquals.Size = New Size(92, 80)
+        btnEquals.TabIndex = 13
+        btnEquals.Text = "="
+        btnEquals.UseVisualStyleBackColor = True
+        ' 
+        ' btnMinus
+        ' 
+        btnMinus.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnMinus.Location = New Point(494, 202)
+        btnMinus.Name = "btnMinus"
+        btnMinus.Size = New Size(92, 75)
+        btnMinus.TabIndex = 14
+        btnMinus.Text = "-"
+        btnMinus.UseVisualStyleBackColor = True
+        ' 
+        ' btnDivide
+        ' 
+        btnDivide.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnDivide.Location = New Point(399, 358)
+        btnDivide.Name = "btnDivide"
+        btnDivide.Size = New Size(93, 80)
+        btnDivide.TabIndex = 15
+        btnDivide.Text = "/"
+        btnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Font = New Font("Microsoft YaHei UI", 9.75F, FontStyle.Bold)
+        btnClear.Location = New Point(305, 358)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(91, 80)
+        btnClear.TabIndex = 16
+        btnClear.Text = "C"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(208, 56)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(384, 64)
+        TextBox1.TabIndex = 17
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Zoom
+        ClientSize = New Size(800, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(btnClear)
+        Controls.Add(btnDivide)
+        Controls.Add(btnMinus)
+        Controls.Add(btnEquals)
+        Controls.Add(btnMultiply)
+        Controls.Add(btnPlus)
+        Controls.Add(btn1)
+        Controls.Add(btn0)
+        Controls.Add(btn9)
+        Controls.Add(btn8)
+        Controls.Add(btn7)
+        Controls.Add(btn6)
+        Controls.Add(btn5)
+        Controls.Add(btn4)
+        Controls.Add(btn3)
+        Controls.Add(btn2)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btnMultiply As Button
+    Friend WithEvents btnEquals As Button
+    Friend WithEvents btnMinus As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ guicalc/Form1.vb
@@ -0,0 +1,47 @@
+﻿Public Class Form1
+    Dim firstValue As Double
+    Dim SecondValue As Double
+    Dim currentoperation As String
+
+    Private Sub btnNumber_click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click, btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
+
+        TextBox1.Text &= DirectCast(sender, Button).Text
+    End Sub
+
+    Private Sub btnOperator_click(sender As Object, e As EventArgs) Handles btnPlus.Click, btnMultiply.Click, btnMinus.Click, btnDivide.Click
+
+        firstValue = Double.Parse(TextBox1.Text)
+        currentoperation = DirectCast(sender, Button).Text
+        TextBox1.Text = ""
+    End Sub
+
+    Private Sub btnEquals_click(sender As Object, e As EventArgs) Handles btnEquals.Click
+        SecondValue = Double.Parse(TextBox1.Text)
+        Select Case currentoperation
+            Case "+"
+                TextBox1.Text = (firstValue + SecondValue).ToString()
+            Case "-"
+                TextBox1.Text = (firstValue - SecondValue).ToString()
+            Case "*"
+                TextBox1.Text = (firstValue * SecondValue).ToString
+            Case "/"
+                If SecondValue = 0 Then
+                    TextBox1.Text = "ERROR"
+                Else
+                    TextBox1.Text = (firstValue / SecondValue).ToString
+                End If
+        End Select
+    End Sub
+
+    Private Sub btnClear_click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        TextBox1.Text = ""
+        firstValue = 0
+        SecondValue = 0
+        currentoperation = ""
+
+    End Sub
+
+
+
+End Class
